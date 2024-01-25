12055ec5512003b5bf254e953711e8913d7c8dab
Taha Smaoui <tahasmaoui1993@gmail.com>
2024-01-25 17:38:14 +0100

    Added constructor to Appjs & Created a Show/Hide button.

--- src/App.js
+++ src/App.js
@@ -1,9 +1,21 @@
 import React, { Component } from 'react';
 import Counter from './Components/Counter';
 export default class App extends Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {show: false};
+  }
+
+  toggle=() => {
+    this.setState({show:!this.state.show})
+  }
+
+
   render() {
     return (
       <div>
+        <button>Show/Hide</button>
         <Counter/>
       </div>
     );
